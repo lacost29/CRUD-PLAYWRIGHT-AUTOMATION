a7d67aaaf0bd5729e30288be1449534a5b3c49f7
Ilie Sulman <illyashulman11@gmail.com>
2024-08-15 15:25:27 +0100

    extract pom in create user tests

--- e2e/tests/createUser.negative.spec.ts
+++ e2e/tests/createUser.negative.spec.ts
@@ -2,14 +2,14 @@ import { test, expect } from "@playwright/test";
 import { UsersPage } from "../pages/usersPage";
 
 test.describe("Create a user validation", () => {
+  let usersPage: UsersPage;
+
   test.beforeEach(async ({ page }) => {
     await page.goto("/");
+    usersPage = new UsersPage(page);
   });
 
-  test("Gets validation error when name length is too short", async ({
-    page,
-  }) => {
-    const usersPage = new UsersPage(page);
+  test("Gets validation error when name length is too short", async () => {
     await expect(usersPage.nameField).toBeVisible();
     await usersPage.nameField.fill("Tom");
     await expect(usersPage.nameFieldValidationError).toHaveText(
@@ -17,10 +17,7 @@ test.describe("Create a user validation", () => {
     );
   });
 
-  test("Gets validation error when name length is too long", async ({
-    page,
-  }) => {
-    const usersPage = new UsersPage(page);
+  test("Gets validation error when name length is too long", async () => {
     await expect(usersPage.nameField).toBeVisible();
     await usersPage.nameField.fill("Mr Tomas Chris Jn");
     await expect(usersPage.nameFieldValidationError).toHaveText(
@@ -28,20 +25,14 @@ test.describe("Create a user validation", () => {
     );
   });
 
-  test("Create button is disabled when name field is empty", async ({
-    page,
-  }) => {
-    const usersPage = new UsersPage(page);
+  test("Create button is disabled when name field is empty", async () => {
     await usersPage.professionField.fill("QA Engineer");
     await usersPage.dobField.fill("1998-07-29");
     await usersPage.dobField.press("Enter");
     await expect(usersPage.addNewUserButton).toBeDisabled();
   });
 
-  test("Create button is disabled when Profession field is empty", async ({
-    page,
-  }) => {
-    var usersPage = new UsersPage(page);
+  test("Create button is disabled when Profession field is empty", async () => {
     await usersPage.nameField.fill("Mr Tomas Chris J");
     await usersPage.dobField.fill("1998-07-29");
     await usersPage.dobField.press("Enter");
